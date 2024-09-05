f22590b3406449769552a6864a5dd18cca6ac7bf
Jaider20527 <jadoncel@ucompensar.edu.co>
2024-09-05 16:21:21 -0500

    Cinicio control de versiones

---
+++ src/main/java/dev/germantovar/springboot/controllers/CustomerController.java
@@ -0,0 +1,24 @@
+package dev.germantovar.springboot.controllers;
+
+
+import dev.germantovar.springboot.entities.Customer;
+import dev.germantovar.springboot.repository.CustomerRepository;
+import dev.germantovar.springboot.services.ICustomerService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
+
+@RestController
+
+public class CustomerController {
+      @Autowired
+      private ICustomerService service;
+
+      @Autowired
+      private CustomerRepository customerRepository;
+
+      @GetMapping("lista")
+      public List<Customer> getAll(){ return service.getAll();}
+}
---
+++ src/main/java/dev/germantovar/springboot/repository/CustomerRepository.java
@@ -0,0 +1,11 @@
+package dev.germantovar.springboot.repository;
+
+import dev.germantovar.springboot.entities.Customer;
+import org.springframework.data.repository.CrudRepository;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface CustomerRepository extends CrudRepository<Customer, Long> {
+
+}
